152fc4cd7b2f07307309f3345e05d583dbf59673
Christelle Quilang <christellequilang@protonmail.com>
2023-05-26 19:37:11 +1200

    Initial Commit

---
+++ Handling Errors/Handling_ErrorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Handling_ErrorsApp.swift
+//  Handling Errors
+//
+//  Created by Christelle Quilang on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Handling_ErrorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
